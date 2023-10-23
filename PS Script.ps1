257523aaa73e22e7c7a6c0b59127b807fae4e936
Mustafa Jaber <mustafa.infosec@gmail.com>
2023-10-23 09:21:30 +0300

    Add files via upload

---
+++ PS Script.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module ActiveDirectory
+$users = Import-Csv -LiteralPath "C:\Users\SuperSalmon\Desktop\PowerShell Spreadsheet.csv"
+foreach ($user in $users)
+{
+    $UserName = $user.UserName
+    $DisplayName = $user.Display
+    $path = $user.Path
+    $Password = $user.Password
+    $upn = $user.PrincipalName
+    $FirstName = $user.FirstName
+    $LastName = $user.LastName
+    $Title = $user.Title
+    New-ADUser -Name $UserName -DisplayName $DisplayName -UserPrincipalName $upn -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
+    -Enabled $true -ChangePasswordAtLogon $false -Path "$path" -Surname $FirstName -GivenName $LastName -Title $Title
+    }
